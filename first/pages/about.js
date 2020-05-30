e71d6cc0ac0ad3f4b1b3a7f2b7132d3897eae5ff
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-30 20:47:17 +0530

    Practicing components

---
+++ first/pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+  console.log("************* About **************");
+
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>Hello!!!</p>
+    </>
+  );
+};
+
+export default About;
---
+++ first/pages/contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  console.log("********** Contact Page ***********");
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Hello There!</p>
+    </div>
+  );
+};
+
+export default Contact;
