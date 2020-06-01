157f0ca3f2bf2ecd749922827af3a16100d4b5d3
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-01 11:37:07 +0530

    removed navbar component

--- first/pages/index.js
+++ first/pages/index.js
@@ -1,10 +1,3 @@
-import NavBar from "../component/NavBar";
-
-const Index = () => (
-  <>
-    <NavBar />
-    <h1>Hello World</h1>
-  </>
-);
+const Index = () => <h1>Hello World</h1>;
 
 export default Index;
